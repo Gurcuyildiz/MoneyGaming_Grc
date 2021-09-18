b3565180e8e8702ccf03483a798b699978fec07f
Gurcuyildiz <gurcuyildiz13@gmail.com>
2021-09-18 16:06:15 +0100

    I added my Web Driver Factory

--- src/test/java/com/moeygaming/Test1.java
+++ src/test/java/com/moeygaming/Test1.java
@@ -1,5 +1,7 @@
 package com.moeygaming;
 
+import com.moeygaming.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -8,6 +10,8 @@ public class Test1 {
 
     @BeforeMethod
     public void setUp(){
+WebDriver driver= WebDriverFactory.getDriver("chrome");
+driver.manage().window().maximize();
 
     }
 
